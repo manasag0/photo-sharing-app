a9a6d1bb361f01c8c47f3a29ec1746cf2d6513e3
Manasa G <manasa.g7890@hotmail.com>
2023-08-14 12:47:40 +0530

    ma

--- photoshare/src/App.js
+++ photoshare/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       {/* <h1>hello World</h1>  */}
 
-      
+
     </div>
   );
 }
